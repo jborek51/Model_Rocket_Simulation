14bbce2e372e3ed19189b8104edb271f33ff8588
John <jborek@uncc.edu>
2020-12-09 12:42:41 -0500

    3D model is functional. Just missing aerodynamics - JB

--- Test Scripts/JohnBorek3D_ts.m
+++ Test Scripts/JohnBorek3D_ts.m
@@ -5,15 +5,16 @@ computer = 1;                           %   Computer reference for John
 loadComponent('noControl');             %   Controller parameters 
 loadComponent('Concord3D');             %   Environment parameters 
 loadComponent('SLI2020_SubScale');      %   Vehicle parameters 
-
 %%  Environment Properties 
 Env.wind0.setValue([5 0 0]*0.621371,'m/s');
 Env.windZ.setValue([10 0 0]*0.621371,'m/s');
-
-
+%%  Vehicle Properties 
+Veh.initPosVecGnd.setValue([0;0;Veh.length.Value-Veh.rCMpad_B.Value(1)],'m');
+Veh.initVelVecBdy.setValue([0;0;0],'m/s');
+Veh.initEulAng.setValue([0;pi/2;0],'rad');
+Veh.initAngVelVec.setValue([0;0;0],'rad/s');
 %%  Run Simulation
 simWithMonitor('Simulation_Model')
 %%  Store and Save Outputs
 tsc = signalcontainer(logsout);
 %%  Plot Results
-Plot_Kinematics(Sim);                   %   Plot altitude, velocity, and acceleration 
